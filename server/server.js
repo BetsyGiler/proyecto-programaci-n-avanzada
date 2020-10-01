915a8177f4e89b4011bc54a5bfc76a872d242445
Joel.Garcia <garciajunior796@gmail.com>
2020-09-30 20:08:18 -0500

    CORS

--- server/server.js
+++ server/server.js
@@ -6,11 +6,8 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-      "Access-Control-Allow-Headers", 
-      "Origin, X-Requested-With, Content-Type, Accept", 
-      "GET,PUT,POST,DELETE,PATCH,OPTIONS"
-    );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   next();
 });
 
