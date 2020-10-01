3b0a59f21a1e32e49c3df92d00daf46be1dda195
Joel.Garcia <garciajunior796@gmail.com>
2020-09-30 19:52:36 -0500

    Otra vez Olynda

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Authorization", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "authorization", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
